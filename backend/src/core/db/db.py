84822bdad4c3e5bb227c31cb66bc7dd76e7877df
Egor Kuzovkov <ekuzovkov@enterrasolutions.com>
2023-02-08 18:22:47 +0100

    add: database configuration

---
+++ backend/src/core/db/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from src.core import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG, future=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+Base = declarative_base()
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
