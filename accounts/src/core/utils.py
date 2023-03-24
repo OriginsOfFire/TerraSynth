379b43f0945daa794351335f2d4d6b19417ca34d
Yahor Kuzaukou <yahor.kuzaukou@innowise-group.com>
2023-03-24 10:37:22 +0100

    fix: change application structure and docker configuration

---
+++ accounts/src/core/utils.py
@@ -0,0 +1,25 @@
+from jose import jwt, ExpiredSignatureError, JWTError
+from passlib.context import CryptContext
+
+from src.core import settings
+from src.core.exception.base_exception import InvalidTokenError
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+async def verify_password(plain_password: str, hashed_password: str):
+    print(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+async def hash_password(plain_password: str) -> str:
+    return pwd_context.hash(plain_password)
+
+
+async def decode_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get('sub')
+    except (ExpiredSignatureError, JWTError, KeyError):
+        raise InvalidTokenError
+    return email
---
+++ accounts/src/rest/api/v1/configuration_router.py
@@ -0,0 +1,47 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from src.core.db.db import get_session
+from src.core.managers.configuration_manager import ConfigurationManager
+from src.models import User
+from src.rest.schemas.configuration_schema import ConfigurationCreateSchema, ConfigurationSchema
+from src.services.auth_service import AuthService
+
+configuration_router = APIRouter(tags=['configurations'])
+
+
+@configuration_router.get('/configuration')
+async def get_configurations(
+        session: AsyncSession = Depends(get_session),
+        user: User = Depends(AuthService.get_current_user),
+) -> list:
+    return await ConfigurationManager.list(
+        session=session,
+        filter_fields={'user_id': user.id}
+    )
+
+
+@configuration_router.post('/configuration/', status_code=status.HTTP_201_CREATED, response_model=ConfigurationSchema)
+async def create_configuration(
+        input_data: ConfigurationCreateSchema,
+        session: AsyncSession = Depends(get_session),
+        user: User = Depends(AuthService.get_current_user),
+):
+    input_data.id = user.id
+
+    return await ConfigurationManager.create(
+        input_data=input_data,
+        session=session
+    )
+
+
+@configuration_router.delete('/configuration/{config_id}', status_code=status.HTTP_200_OK)
+async def delete_configuration(
+        config_id: int,
+        session: AsyncSession = Depends(get_session),
+        user: User = Depends(AuthService.get_current_user)
+):
+    return await ConfigurationManager.delete(session=session, id=config_id)
