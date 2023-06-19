d17b424509377115a811d6d8a91df4d845da0157
Yahor Kuzaukou <e.kuzovkov@digiteum.com>
2023-06-19 23:15:47 +0200

    add: id return for resources

--- backend/configurations/src/rest/schemas/resource_schema.py
+++ backend/configurations/src/rest/schemas/resource_schema.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 
 from src.models.argument_model import AttributeModel, ArgumentModel, InitArgumentModel, InitAttributeModel
+from src.models.base_model import BaseMongoModel
 
 
-class ResourceSchema(BaseModel):
+class ResourceSchema(BaseMongoModel):
     provider_id: str
     name: str
     arguments: list[ArgumentModel]
     attributes: list[AttributeModel]
 
 
-class InitResourceSchema(BaseModel):
+class InitResourceSchema(BaseMongoModel):
     resource_id: str
     name: str
     configuration_id: int
